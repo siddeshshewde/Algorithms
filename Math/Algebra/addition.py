95eb10b10258fb72aef1bddde77dad042516582f
siddeshshewde <siddesh.darkeyed95@gmail.com>
2020-08-30 20:36:57 +0530

    Add files via upload

---
+++ Math/Algebra/addition.py
@@ -0,0 +1,18 @@
+
+
+def addition(a, b):
+    """Additon of two numbers"""
+    
+    """ 
+    >>> addition(2, 2)
+    4
+    
+    >>> addition(2, -2)
+    0
+    """
+    return a + b
+
+if __name__ == "__main__":
+    a = 5
+    b = 6
+    print(f"The sum of {a} + {b} is {addition(a, b)}")
