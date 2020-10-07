46be4dbabf594676f2c3d85bb5f93ec3266c0912
noobkid2411 <69161722+noobkid2411@users.noreply.github.com>
2020-10-07 09:11:49 +0530

    Update factorial.c

--- C/Recursive Programs/factorial.c
+++ C/Recursive Programs/factorial.c
@@ -4,13 +4,12 @@ int fact(int);
 
 int main()
 {
-    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
     int num, f;
     printf("\n\nEnter a number: ");
     scanf("%d", &num);
     f= fact(num);
     printf("\n\nFactorial of  %d is  %d\n\n", num, f);
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+  
     return 0;
 }
 
